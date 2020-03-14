b11a923bd691caf9dded301e94580cb948f7dc5e
529038378 <529038378@qq.com>
2020-03-14 16:52:24 +0800

    IFFT框架实现

---
+++ Assets/FFTOcean/Script/CommonData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommonData
+{
+    static public string ComputeKernelName = "Main";
+    static public string ComputeInputBufferName = "InputBuffer";
+    static public string ComputeOutputBufferName = "OutputBuffer";
+    static public Vector3Int ComputeThreadSize = new Vector3Int(1024, 1024, 1);
+    static public Vector2Int TexSize = new Vector2Int(1024, 1024);
+}
---
+++ Assets/FFTOcean/Script/IFFTUtil.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+class IFFTUtil
+{
+    #region var
+    public struct InitParam
+    {
+        public int RTid;    //用来更新到最好的RT id
+        public ComputeShader ComputeShader; //用来并行计算的shader
+        public int Size; //输入变量大小
+        public List<int> Input;
+        public string PingTexName;
+        public string PongTexName;
+    }
+    public bool Done
+    {
+        get;
+        private set;
+    }
+    InitParam m_param;
+    int m_stage_count = 0;
+
+    int m_kernel;
+    #endregion
+
+    #region  method
+    public void Init(in InitParam param)
+    {
+        m_param = param;
+        InitComputeShaderData();
+        OnInit();
+    }
+
+    void InitComputeShaderData()
+    {
+        m_kernel = m_param.ComputeShader.FindKernel(CommonData.ComputeKernelName);
+    }
+
+    public void Begin()
+    {
+        Done = false;
+    }
+
+    public void Update()
+    {
+        for(int i=0; i<m_stage_count; ++i)
+        {
+            CalStageOutput((i % 2) == 0);
+        }
+        OnDone();
+    }
+
+    void CalStageOutput(bool even)
+    {
+        //设置ping pong坐标翻转操作
+        if(even)
+        {
+            m_param.ComputeShader.SetTextureFromGlobal(m_kernel, CommonData.ComputeInputBufferName, m_param.PingTexName);
+            m_param.ComputeShader.SetTextureFromGlobal(m_kernel, CommonData.ComputeInputBufferName, m_param.PongTexName);
+        }
+        else
+        {
+            m_param.ComputeShader.SetTextureFromGlobal(m_kernel, CommonData.ComputeInputBufferName, m_param.PongTexName);
+            m_param.ComputeShader.SetTextureFromGlobal(m_kernel, CommonData.ComputeInputBufferName, m_param.PingTexName);
+        }
+        m_param.ComputeShader.Dispatch(m_kernel, CommonData.ComputeThreadSize.x, CommonData.ComputeThreadSize.y, CommonData.ComputeThreadSize.z);
+    }
+
+    void OnDone()
+    {
+        Done = true;
+    }
+
+    void OnInit()
+    {
+        m_stage_count = (int)(Mathf.Log(2, m_param.Size));
+    }
+    #endregion
+    
+}
